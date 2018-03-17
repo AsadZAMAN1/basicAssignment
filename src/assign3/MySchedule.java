351ca71e9001a188a3ba61e9786a2ff579da5ef1
AsadZAMAN1 <ijazasad20@gmail.com>
2018-03-17 22:37:03 +0500

    3    DONE

---
+++ src/assign3/MySchedule.java
@@ -0,0 +1,34 @@
+package assign3;
+
+public class MySchedule {
+    public static void main(String args[]) {
+        String course1 = "Complex analysis";
+        String course2 = "Microprocessors";
+        String course3 = "Instrumentation";
+        String course4 = "Basic Mechanical engineering";
+        String course5 = "DC Machines";
+        String course6 = "Linear Algebra";
+        String course7 = "Electronic devices";
+        String course8 = "Data Structures";
+        String teacher1 = "Miss Aisha Tabassum";
+        String teacher2 = "Engr. Irfan Qaisar ";
+        String teacher3 = "Engr. Mubeen Khan";
+        String teacher4 = "Dr. Sami Ullah";
+        String teacher5 = "Engr. Nazam Siddique";
+        String teacher6 = "Miss Yumna Bilal ";
+        String teacher7 = "Dr. Shahid Iqbal";
+        String teacher8 = "Sir. Osman Aslam";
+        System.out.println("+-----------------------------------------------------------------+");
+        System.out.println("| 1 |  " + course1 + "             | " + teacher1 + "         |");
+        System.out.println("| 2 |  " + course2 + "              | " + teacher2 + "         |");
+        System.out.println("| 3 |  " + course3 + "              | " + teacher3 + "           |");
+        System.out.println("| 4 |  " + course4 + " | " + teacher4 + "              |");
+        System.out.println("| 5 |  " + course5 + "                  | " + teacher5 + "        |");
+        System.out.println("| 6 |  " + course6 + "               | " + teacher6 + "           |");
+        System.out.println("| 7 |  " + course7 + "           | " + teacher7 + "            |");
+        System.out.println("| 8 |  " + course8 + "              | " + teacher8 + "            |");
+        System.out.println("+-----------------------------------------------------------------+");
+
+
+    }
+}
